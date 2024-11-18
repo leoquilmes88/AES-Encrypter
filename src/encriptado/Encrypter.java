9cc9f46e434c7d4b6e17850f3e50ba44d6707996
leoquilmes88 <leoquilmes88@gmail.com>
2024-11-18 10:39:42 -0300

    algunos inlines

--- src/encriptado/Encrypter.java
+++ src/encriptado/Encrypter.java
@@ -2,28 +2,21 @@ package encriptado;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.SecureRandom;
 import java.security.spec.KeySpec;
 
 public class Encrypter {
-    private final static String algoritmo = "AES";
-    private final static String tipoCifrado = "AES/CBC/PKCS5Padding";
 
     public static byte[] encrypt(String key, byte[] input) throws Exception {
-        Cipher cipher = Cipher.getInstance(tipoCifrado);
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         SecretKeyFactory secretFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-        SecureRandom random = new SecureRandom();
-        byte[] iv = new byte[16];
         byte[] salt = new byte[32];
-        random.nextBytes(iv);
-        random.nextBytes(salt);
+        new SecureRandom().nextBytes(salt);
         KeySpec keySpec = new PBEKeySpec(key.toCharArray(), salt, 65536, 256);
-        SecretKeySpec secretKeySpec = new SecretKeySpec(secretFactory.generateSecret(keySpec).getEncoded(), algoritmo);
-        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
-        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secretFactory.generateSecret(keySpec).getEncoded(), "AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
         return cipher.doFinal(input);
     }
 
