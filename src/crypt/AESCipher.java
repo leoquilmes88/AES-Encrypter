915ca24c9cccf9c352b2eab483f94949af20bd96
leoquilmes88 <leoquilmes88@gmail.com>
2024-11-21 22:36:15 -0300

    correccion de falta de interpretacion de salt a byte array

--- src/crypt/AESCipher.java
+++ src/crypt/AESCipher.java
@@ -1,11 +1,12 @@
 package crypt;
 
+import org.apache.commons.codec.binary.Hex;
+
 import javax.crypto.Cipher;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
 import java.security.spec.KeySpec;
 
 public class AESCipher {
@@ -16,7 +17,7 @@ public class AESCipher {
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         SecretKeyFactory secretFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
         KeySpec keySpec = new PBEKeySpec(password.toCharArray(),
-                salt.getBytes(StandardCharsets.UTF_8), 65536, 256);
+                Hex.decodeHex(salt), 65536, 256);
         SecretKeySpec secretKeySpec = new SecretKeySpec(secretFactory.generateSecret(keySpec).getEncoded(),
                 "AES");
         cipher.init(actionMode.getCipherMode(), secretKeySpec, ivParameter);
